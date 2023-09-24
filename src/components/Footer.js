248beed4c67fd87d73691e5cbda968ca6c3549cb
Arhci <arlan.abkeyev@mail.ru>
2023-09-25 02:59:03 +0300

    add css

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            Все права защещены &copy;
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <span className="logo">House Staff</span>
+                <ul className='nav'>
+                    <li>Про нас</li>
+                    <li>Контакты</li>
+                    <li>Кабинет</li>
+                </ul>
+            </div>
+            <div className="presentation">
+
+            </div>
+        </header>
+    );
+};
+
+export default Header;
